4b74b546cc0e8cff9586b94a9bad758545a671b4
susanataranco <tarancosusana@gmail.com>
2026-07-16 09:26:49 +0200

    add persistent layout with Outlet for shared 3D canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import styled from 'styled-components'
+import { Outlet } from 'react-router-dom'
 import TestCube from './scene/TestCube'
 
-const Title = styled.h1`
-  color: ${({ theme }) => theme.colors.accent};
-  font-family: ${({ theme }) => theme.font.body};
-    
-`
 function App() {
     return (
         <>
-            <Title>UI Dev</Title>
             <TestCube/>
+
+            <Outlet />
         </>
     );
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,18 +2,23 @@ import { createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home'
 import Bio from './pages/Bio'
 import ProjectDetail from './pages/ProjectDetail'
+import App from "./App.tsx";
 
-export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Home />,
-    },
-    {
-        path: '/bio',
-        element: <Bio />,
-    },
-    {
-        path: '/proyect/:id',
-        element: <ProjectDetail />,
-    },
-])
+export const router = createBrowserRouter([{
+    path: "/",
+    element: <App />,
+    children: [
+        {
+            path: '/',
+            element: <Home/>,
+        },
+        {
+            path: '/bio',
+            element: <Bio/>,
+        },
+        {
+            path: '/proyect/:id',
+            element: <ProjectDetail/>,
+        },
+    ]
+}])
